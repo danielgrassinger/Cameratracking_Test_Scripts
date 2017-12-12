d4369eb45ffa8fe77eb40ff2e0988edc19af2ec2
Daniel Grassinger <DanielGrassinger@gmail.com>
2017-12-12 14:19:12 +0100

    Works now with the specified interface

--- Fake_Cam_Signal.py
+++ Fake_Cam_Signal.py
@@ -1,26 +1,32 @@
+'''
+Sends af fake signal that two tools were taken out of the toolbox and returned after a few seconds
+@author Daniel Grassinger
+'''
 from time import sleep
-import datetime
 import requests
 
-def takenFromToolbox(id, time):
-    r = requests.get('https://github.com/')
+def takenFromToolbox(id):
+    command = 'http://localhost:8000/cgi-bin/DB_RequestHandler.py?command=takeTool&id='+ str(id)
+    r = requests.get(command)
     print(r.status_code)
+    print(r.text)
 
-def retrunedToToolbox(id, time):
-    pass
+def retrunedToToolbox(id):
+    command = 'http://localhost:8000/cgi-bin/DB_RequestHandler.py?command=returnTool&id=' + str(id)
+    r = requests.get(command)
+    print(r.status_code)
+    print(r.text)
 
 
 def main():
 
-
-
-    takenFromToolbox(12,datetime.datetime.now())
+    takenFromToolbox(12)
     sleep(3)
-    retrunedToToolbox(12, datetime.datetime.now())
+    retrunedToToolbox(12)
 
-    takenFromToolbox(11, datetime.datetime.now())
+    takenFromToolbox(11)
     sleep(5)
-    retrunedToToolbox(11, datetime.datetime.now())
+    retrunedToToolbox(11)
 
 if __name__ == "__main__":
     main()
